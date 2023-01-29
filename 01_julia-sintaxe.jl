8c30b545fc9294a6f19cc2dc9cdd0055f9c4f5fc
Bernardo <mrochabernardo@gmail.com>
2023-01-29 12:06:51 -0300

    Correção de pequenos erros

--- 01_julia-sintaxe.jl
+++ 01_julia-sintaxe.jl
@@ -1,10 +1,10 @@
 s1 = "isso é uma string"
 s2 = """ também é  uma string """
-print(typeof('a'))
-print(s1)
-print(s2)
+println(typeof('a')) # Char
+println(s1) # isso é uma string
+println(s2) #  também é  uma string
 nome = "Bernardo"
 dedos = 10
 
 print("Olá, meu nome é $nome, eu tenho $dedos dedos") 
-# Charisso é uma string também é  uma string Olá, meu nome é Bernardo, eu tenho 10 dedos
+# Olá, meu nome é Bernardo, eu tenho 10 dedos
--- 02_operadores-matematicos.jl
+++ 02_operadores-matematicos.jl
@@ -1,6 +1,24 @@
+println(2 + 3) # 5
+println(2 ^ 3) # 8
+println(2.3 / 7) # 0.32857142857142857
+println(3.6 * 2.9) # 10.44
+println(2.7 ^ 1.3) # 3.6372478296536
+println(log(8)) # 2.0794415416798357
+println(log10(8)) # 0.9030899869919435
+
 sqrt(9)
 
-println(sqrt(16))
+sin(30)
+cos(45) # 0.5253219888177297
+tan(30) # -6.405331196646276
+
+
+"""
+julia> pi
+π = 3.1415926535897...
+"""
+
+println(sqrt(16)) # 4
 println(sqrt(complex(-25))) # 0.0 + 5.0im
 
 #println(sqrt(-49)) # DomainError with -49.0:
@@ -9,12 +27,12 @@ println(sqrt(complex(-25))) # 0.0 + 5.0im
 # Julia program to illustrate 
 # the use of isqrt() method
   
-# Getting integer square root of the specified value.
-println(isqrt(12))
-println(isqrt(17))
-println(isqrt(5))
-println(isqrt(227))
+# Getting integer square root of the specified value. 
+# Valor inteiro mais próximo
+println(isqrt(12)) # 3
+println(isqrt(17)) # 4
+println(isqrt(5)) # 2
+println(isqrt(227)) # 15 
 
 # Raiz cúbica
-
 cbrt(8) # 2.0
