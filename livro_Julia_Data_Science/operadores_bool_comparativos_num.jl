c8a638f26e8cd39f34b1d77d21628aea865b0a51
bernardo monteiro rocha <mrochabernardo@gmail.com>
2024-03-30 12:07:28 -0300

    Comparações com números

---
+++ livro_Julia_Data_Science/operadores_bool_comparativos_num.jl
@@ -0,0 +1,18 @@
+# !: NOT
+# &&: AND
+# ||: OR
+
+!true
+!true
+
+(false && true) || (!false)
+
+(6 isa Int64) && (6 isa Real)
+
+(true isa Int64)
+
+# Com números
+1 == 1
+1 >= 10
+1 == 1.0
+(1 != 10) || (3.14 <= 2.71)
